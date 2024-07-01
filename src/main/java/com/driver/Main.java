afbe22a840494af818d7114c8c80da12afb1b277
jais1441 <jaiswalvikas7276@gmail.com>
2024-07-01 14:43:35 +0530

    Task Complete

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,19 +1,22 @@
 package com.driver;
 
 
-class A {
-    public String meth() {
+class A{
+
+    public String meth(){
         return "Invoking method from class A";
     }
+
 }
 
-class B extends A {
+class B extends A{
 
-    public String meth() {
+    public String meth(){
+
         return "Method is overridden in Extendend class B";
     }
-}
 
+}
 
 public class Main {
 
