2d32531721965a1da016b1e2981d68d02f0da23e
jais1441 <jaiswalvikas7276@gmail.com>
2024-08-01 19:22:47 +0530

    reset

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,27 +1,8 @@
 package com.driver;
 
-
-class A {
-    public String meth() {
-        return "Invoking method from class A";
-    }
-}
-
-// Task 2 & 4: Class B extending A and overriding meth
-class B extends A {
-    @Override
-    public String meth() {
-        return "Method is overridden in Extended class B";
-    }
-}
-
 public class Main {
     public static void main(String[] args){
 
-
-        B b = new B();
-        // Calling method meth from class A using an object of class B
-        System.out.println(b.meth());
     }
   
 }
