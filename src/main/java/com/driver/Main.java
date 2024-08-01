6da6daf83bab8a390a6961e74ef0d2ac360d55a5
jais1441 <jaiswalvikas7276@gmail.com>
2024-08-01 19:48:34 +0530

    Task three

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,6 +3,10 @@ package com.driver;
 public class Main {
     public static void main(String[] args){
 
+
+        B b = new B();
+        // Calling method meth from class A using an object of class B
+        System.out.println(b.meth());
     }
   
 }
