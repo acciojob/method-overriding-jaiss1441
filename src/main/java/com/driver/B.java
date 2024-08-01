cf11ea3749bf4efb1a7347a2cea9225d2185878e
jais1441 <jaiswalvikas7276@gmail.com>
2024-08-01 19:47:35 +0530

    Task two

--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -1,9 +1,6 @@
 package com.driver;
 
-// Task 2 & 4: Class B extending A and overriding meth
 public class B extends A {
-    @Override
-    public String meth() {
-        return "Method is overridden in Extended class B";
-    }
+    // Class B will inherit method meth from class A
 }
+
