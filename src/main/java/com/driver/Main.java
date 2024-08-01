37321659201291ba1899302b999e62e3018941cd
jais1441 <jaiswalvikas7276@gmail.com>
2024-08-01 19:46:31 +0530

    Task One

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,11 +1,8 @@
 package com.driver;
 
-
-// Task 3 & 5: Main class to create an object and call the method
 public class Main {
-    public static void main(String[] args) {
-        B b = new B();
-        // This will call the overridden method in class B
-        System.out.println(b.meth()); // Output: Method is overridden in Extended class B
+    public static void main(String[] args){
+
     }
+  
 }
