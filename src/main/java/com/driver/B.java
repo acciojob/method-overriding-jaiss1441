ef8784e60a86f6b8fa2e4a65f6077adafcac09f6
jaiss1441 <jaiswalvikas7276@gmail.com>
2024-09-03 18:20:52 +0530

    changes done

--- src/main/java/com/driver/B.java
+++
@@ -1,9 +0,0 @@
-package com.driver;
-
-// Task 2 & 4: Class B extending A and overriding meth
-public class B extends A {
-    @Override
-    public String meth() {
-        return "Method is overridden in Extended class B";
-    }
-}
